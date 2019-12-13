377ab7658568928c4799710d2e7b96af0964f9a0
wyao905 <warrenyca905@gmail.com>
2019-12-12 21:11:25 -0500

    built out donation_status function for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,21 @@ class User < ApplicationRecord
     has_many :exhibits, through: :donations
     has_many :animals, through: :actions
 
-    # def donation_status=(donations)
-    # end
+    def donation_status
+        if self.zookeeper
+            "Zookeepers do not have access to donation functions and benefits."
+        else
+            sum = 0
+            self.donations.each{|donation| sum += donation.amount}
+            if sum >= 100000
+                "Platinum"      # user can claim/create an exhibit and appoint zookeepers
+            elsif sum >= 10000
+                "Gold"          # user can adopt up to 5 animals
+            elsif sum >= 1000
+                "Silver"        # user can adopt an animal (name, feed, see list of actions for animal)
+            else
+                "Bronze"        # everyone starts off at bronze
+            end
+        end
+    end
 end
