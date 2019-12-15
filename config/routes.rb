b3e6a6d72dc7db48b0db06e7392eb72cb84e0da6
wyao905 <warrenyca905@gmail.com>
2019-12-15 00:07:38 -0500

    created donation form on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users do
+    resources :donations
+    
     get '/claim', to: 'exhibits#claim'
   end
   
-  resources :animals, :exhibits, :donations
+  resources :animals, :exhibits
 
   get '/', to: 'welcome#home'
 
