697739979f1c2dce7a331312b79aa520945d0cc1
wyao905 <warrenyca905@gmail.com>
2019-12-16 00:05:18 -0500

    changed nested form for donations to un-nested, created exhibits new view, built out donations create action

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -6,16 +6,17 @@ class DonationsController < ApplicationController
     end
 
     def create
-        binding.pry
-
-
-        # {"donation"=>{"amount"=>"100",
-        #               "message"=>"test",
-        #               "exhibit_id"=>"1"},
-        #  "commit"=>"Create Donation",
-        #  "controller"=>"donations",
-        #  "action"=>"create",
-        #  "user_id"=>"1"}
+        donation = current_user.donations.build(donation_params(:amount, :message, :exhibit_id))
+        if donation.save
+            new_balance = current_user.balance - donation.amount
+            new_fund = donation.exhibit.funds + donation.amount
+            current_user.update(balance: new_balance)
+            donation.exhibit.update(funds: new_fund)
+            binding.pry
+            redirect_to user_path(current_user)
+        else
+            render "users/show"
+        end
     end
 
     def edit
@@ -25,5 +26,15 @@ class DonationsController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def donation_params(*args)
+        params.require(:donation).permit(*args)
+    end
+
+    def current_user
+        User.find(session[:user_id])
     end
 end
