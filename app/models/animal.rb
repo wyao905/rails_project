8b4beb4ffe59facd956338d9bd441eb18075318f
wyao905 <warrenyca905@gmail.com>
2019-12-16 20:14:57 -0500

    added mood, how_hungry, and how_healthy methods to animal object, built more of show view for animals

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,49 @@ class Animal < ApplicationRecord
     belongs_to :exhibit
     has_many :actions
     has_many :users, through: :actions
+
+    def mood
+        case self.happiness
+        when 5
+            "#{self.name} seems to be enjoying life."
+        when 4
+            "#{self.name} looks quite pleased."
+        when 3
+            "#{self.name} seems unperturbed."
+        when 2
+            "#{self.name} seems slightly grumpy."
+        else
+            "#{self.name} does not look happy at all."
+        end
+    end
+
+    def how_hungry
+        case self.hunger
+        when 5
+            "#{self.name} looks like it just had a large meal."
+        when 4
+            "#{self.name} does not seem hungry."
+        when 3
+            "#{self.name} looks well off."
+        when 2
+            "#{self.name} seems slightly peckish."
+        else
+            "#{self.name} looks like it's starving."
+        end
+    end
+
+    def how_healthy
+        case self.health
+        when 5
+            "#{self.name} looks as fit as ever."
+        when 4
+            "#{self.name} seems to be in good shape."
+        when 3
+            "#{self.name} looks average."
+        when 2
+            "#{self.name} looks uncomfortable."
+        else
+            "#{self.name} looks very sick."
+        end
+    end
 end
