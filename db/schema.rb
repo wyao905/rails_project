5dac5d2414606a9bf03e57e0b7e2efa1dd41db2d
wyao905 <warrenyca905@gmail.com>
2020-01-09 15:20:39 -0500

    added column for user id for google oauth to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_033036) do
+ActiveRecord::Schema.define(version: 2020_01_09_201844) do
 
   create_table "actions", force: :cascade do |t|
     t.string "action_type"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_033036) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
 end
